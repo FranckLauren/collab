9e0090f516fe61923b857229a78f5f868fa3a419
Franck Lauren <franckb799@gmail.com>
2024-05-10 15:39:57 +0100

    initial commit

---
+++ lib/vues/presentation.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Presentation extends StatefulWidget {
+  const Presentation({super.key});
+
+  @override
+  State<Presentation> createState() => _PresentatioState();
+}
+
+class _PresentatioState extends State<Presentation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
